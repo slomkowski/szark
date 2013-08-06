bc495def853b31965a3340ee90db456aa3e418ca
Michał Słomkowski <m.slomkowski@gmail.com>
2013-08-06 23:55:45 +0200

    Added I2C expander interface.

---
+++ firmware/bridge/expander.cpp
@@ -0,0 +1,42 @@
+/*
+ * expander.cpp
+ *
+ *  Created on: 06-08-2013
+ *      Author: michal
+ */
+
+#include "i2c.h"
+#include "expander.h"
+
+static const uint8_t EXPANDER_ADDRESS = 0x64;
+
+static const uint8_t WRITE_AND_TRANSFER_DATA = 0x44;
+//static const uint8_t WRITE_DATA = 0x11;
+//static const uint8_t TRANSFER_DATA = 0x22;
+static const uint8_t READ_DATA = 0x11;
+
+void expander::setValue(uint8_t data) {
+	i2c::start();
+
+	i2c::write(i2c::addressToWrite(EXPANDER_ADDRESS));
+	i2c::write(WRITE_AND_TRANSFER_DATA);
+	i2c::write(data);
+
+	i2c::stop();
+}
+
+uint8_t expander::getValue() {
+
+	i2c::start();
+
+	i2c::write(i2c::addressToWrite(EXPANDER_ADDRESS));
+	i2c::write(READ_DATA);
+
+	i2c::start();
+	i2c::write(i2c::addressToRead(EXPANDER_ADDRESS));
+	uint8_t val = i2c::read(i2c::NACK);
+
+	i2c::stop();
+
+	return val;
+}
---
+++ firmware/bridge/expander.h
@@ -0,0 +1,16 @@
+/*
+ * expander.h
+ *
+ *  Created on: 06-08-2013
+ *      Author: michal
+ */
+
+#ifndef EXPANDER_H_
+#define EXPANDER_H_
+
+namespace expander {
+	void setValue(uint8_t data);
+	uint8_t getValue();
+}
+
+#endif /* EXPANDER_H_ */
