da2c1a681a245dd0bd6cf31b67eeef6f0d089d0e
Michał Słomkowski <m.slomkowski@gmail.com>
2014-05-09 00:34:40 +0200

    server: Add wallaroo::Catalog to main().

--- host/server/src/main.cpp
+++ host/server/src/main.cpp
@@ -13,9 +13,34 @@
 #include <stdexcept>
 
 #include <log4cpp/PropertyConfigurator.hh>
+#include <wallaroo/catalog.h>
+
+#include "Configuration.hpp"
+#include "NetServer.hpp"
+
+using namespace std;
+using namespace wallaroo;
 
 int main(int argc, char *argv[]) {
 	std::string initFileName = "logger.properties";
 	log4cpp::PropertyConfigurator::configure(initFileName);
+
+	config::Configuration::create();
+
+	Catalog c;
+	c.Create("comm", "USBCommunicator");
+	c.Create("netServer", "NetServer");
+	c.Create("reqQueuer", "RequestQueuer");
+	c.Create("bridgeProc", "BridgeProcessor");
+
+	use(c["comm"]).as("communicator").of(c["bridgeProc"]);
+	use(c["reqQueuer"]).as("requestQueuer").of(c["netServer"]);
+	use(c["bridgeProc"]).as("requestProcessors").of(c["reqQueuer"]);
+
+	c.CheckWiring();
+
+	auto netServer = shared_ptr<processing::INetServer>(c["netServer"]);
+
+	netServer->run();
 }
 
