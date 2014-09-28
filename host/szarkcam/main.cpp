824fb77e59e8f97e59794fc90bf8fce2775c9029
Michał Słomkowski <m.slomkowski@gmail.com>
2014-09-29 00:35:33 +0200

    szarkcam: Add sending images via UDP.

--- host/szarkcam/main.cpp
+++ host/szarkcam/main.cpp
@@ -3,9 +3,14 @@
 #include <functional>
 #include <future>
 
+#include <boost/asio.hpp>
 #include <opencv2/opencv.hpp>
 
+using namespace boost;
+using boost::asio::ip::udp;
+
 using namespace std::chrono;
+const int MAX_PACKET_SIZE = 1200;
 
 std::chrono::microseconds measureTime(std::function<void()> func) {
 	using std::chrono::duration_cast;
@@ -21,7 +26,72 @@ std::chrono::microseconds measureTime(std::function<void()> func) {
 	return duration_cast<microseconds>(timerEnd - timerBegin);
 }
 
+void doReceive(udp::socket &udpSocket, cv::VideoCapture &leftVideoCapture, cv::VideoCapture &rightVideoCapture, std::unique_ptr<char> &buff, udp::endpoint &recvSenderEndpoint) {
+	udpSocket.async_receive_from(asio::buffer(buff.get(), MAX_PACKET_SIZE), recvSenderEndpoint,
+			[&](system::error_code ec, size_t bytes_recvd) {
+				cv::Mat leftFrame;
+				cv::Mat rightFrame;
+
+				auto duration = measureTime([&]() {
+
+					auto rightJob = std::async(std::launch::async, [&]() -> bool {
+						return rightVideoCapture.read(rightFrame);
+					});
+
+					bool leftSuccess = leftVideoCapture.read(leftFrame);
+					bool rightSuccess = rightJob.get();
+				});
+
+				//std::cout << "Duration frame grabbing: " << duration.count() << " us" << std::endl;
+
+				cv::vector<unsigned char> buf;
+
+				auto duration2 = measureTime([&]() {
+
+					cv::flip(leftFrame, leftFrame, 0);
+					cv::transpose(leftFrame, leftFrame);
+					cv::flip(leftFrame, leftFrame, 0);
+
+					cv::transpose(rightFrame, rightFrame);
+
+					cv::Size sizeLeft = leftFrame.size();
+					cv::Size sizeRight = rightFrame.size();
+
+					cv::Mat im3(sizeLeft.height, sizeLeft.width + sizeRight.width, CV_8UC3);
+					cv::Mat left(im3, cv::Rect(0, 0, sizeLeft.width, sizeLeft.height));
+					leftFrame.copyTo(left);
+					cv::Mat right(im3, cv::Rect(sizeLeft.width, 0, sizeRight.width, sizeRight.height));
+					rightFrame.copyTo(right);
+
+					//cv::imshow("im3", im3);
+					std::vector<int> compression_params; //vector that stores the compression parameters of the image
+					compression_params.push_back(CV_IMWRITE_JPEG_QUALITY); //specify the compression technique
+
+					compression_params.push_back(45);
+					//cv::imwrite("out.jpg", im3, compression_params);
+
+					cv::imencode(".jpg", im3, buf, compression_params);
+				});
+				//std::cout << "Duration processing: " << duration2.count() << " us" << std::endl;
+
+				udpSocket.async_send_to(boost::asio::buffer(&buf[0], buf.size()), recvSenderEndpoint,
+						[&buf](system::error_code ec, size_t bytes_sent) {
+							//std::cout << "buf size " << buf.size() << ", sent " << bytes_sent << std::endl;
+						});
+
+				doReceive(udpSocket, leftVideoCapture, rightVideoCapture, buff, recvSenderEndpoint);
+			});
+}
+
+
 int main() {
+
+	boost::asio::io_service ioService;
+	boost::asio::ip::udp::socket udpSocket(ioService);
+
+	boost::system::error_code err;
+	udpSocket.open(udp::v4());
+	udpSocket.bind(udp::endpoint(udp::v4(), 10192), err);
 
 	cv::VideoCapture leftVideoCapture(0);
 	cv::VideoCapture rightVideoCapture(1);
@@ -38,54 +108,19 @@ int main() {
 		return -1;
 	}
 
-	double dWidth = leftVideoCapture.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
-	double dHeight = leftVideoCapture.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
+	leftVideoCapture.set(CV_CAP_PROP_FRAME_WIDTH, 352);
+	leftVideoCapture.set(CV_CAP_PROP_FRAME_HEIGHT, 288);
 
-	std::cout << "Frame size : " << dWidth << " x " << dHeight << std::endl;
+	rightVideoCapture.set(CV_CAP_PROP_FRAME_WIDTH, 352);
+	rightVideoCapture.set(CV_CAP_PROP_FRAME_HEIGHT, 288);
 
-	while (1) {
-		cv::Mat leftFrame;
-		cv::Mat rightFrame;
 
-		auto duration = measureTime([&]() {
+	std::unique_ptr<char> buff;
+	buff.reset(new char[MAX_PACKET_SIZE]);
+	boost::asio::ip::udp::endpoint recvSenderEndpoint;
 
-			auto rightJob = std::async(std::launch::async, [&]() -> bool {
-				return rightVideoCapture.read(rightFrame);
-			});
+	doReceive(udpSocket, leftVideoCapture, rightVideoCapture, buff, recvSenderEndpoint);
 
-			bool leftSuccess = leftVideoCapture.read(leftFrame);
-			bool rightSuccess = rightJob.get();
-		});
-
-		std::cout << "Duration frame grabbing: " << duration.count() << " us" << std::endl;
-
-		auto duration2 = measureTime([&]() {
-
-			cv::flip(leftFrame, leftFrame, 0);
-			cv::transpose(leftFrame, leftFrame);
-			cv::flip(leftFrame, leftFrame, 0);
-
-			cv::transpose(rightFrame, rightFrame);
-
-			cv::Size sizeLeft = leftFrame.size();
-			cv::Size sizeRight = rightFrame.size();
-
-			cv::Mat im3(sizeLeft.height, sizeLeft.width + sizeRight.width, CV_8UC3);
-			cv::Mat left(im3, cv::Rect(0, 0, sizeLeft.width, sizeLeft.height));
-			leftFrame.copyTo(left);
-			cv::Mat right(im3, cv::Rect(sizeLeft.width, 0, sizeRight.width, sizeRight.height));
-			rightFrame.copyTo(right);
-
-			cv::imshow("im3", im3);
-		});
-		std::cout << "Duration processing: " << duration2.count() << " us" << std::endl;
-
-		if (cv::waitKey(30) == 27) {
-			std::cout << "esc key is pressed by user" << std::endl;
-			break;
-		}
-	}
-
-	return 0;
-
+	ioService.run();
 }
+
