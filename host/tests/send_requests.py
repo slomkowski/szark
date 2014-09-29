a18a9698ac098c8927f34e568948a0b193ef825b
Michał Słomkowski <m.slomkowski@gmail.com>
2014-09-30 00:03:04 +0200

    Update send_requests script.

--- host/tests/send_requests.py
+++ host/tests/send_requests.py
@@ -4,10 +4,20 @@
 Script sends the JSON command to the server many times.
 """
 
+import sys
 import json
 import socket
 
-request = { "killswitch": False, "serial" : 0, "motor" : { "left" : { "dir" : "forward", "speed":4}}}
+if sys.argv[2] == "false":
+    val = False
+else:
+    val = True
+
+#request = { "killswitch": False, "serial" : 0, "motor" : { "left" : { "dir" : sys.argv[2], "speed":int(sys.argv[3])}}}
+#request = { "killswitch": False, "serial" : 0, "arm" : { "elbow" : { "dir" : sys.argv[2], "speed":int(sys.argv[3])}}}
+request = { "killswitch": False, "serial" : 0, "light" : { "left" : val}}
+#request = { "killswitch": False, "serial" : 0, "lcd" : "hello world xfdsfs" }
+
 
 udpAddress = '127.0.0.1'
 udpPort = 10191
@@ -17,8 +27,10 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 jsonText = json.dumps(request)
 print("JSON: " + jsonText)
 
-for i in range(5000):
+for i in range(int(sys.argv[1])):
     jsonText = json.dumps(request)
     sock.sendto(jsonText.encode("UTF-8"), (udpAddress, udpPort))
     request['serial'] = i
+    #request['lcd'] =request['lcd'] + str(i)
+
 
