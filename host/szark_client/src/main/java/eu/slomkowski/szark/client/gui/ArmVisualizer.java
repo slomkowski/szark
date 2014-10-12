ccaf9b0ca68f384286450e1de48124725f20eeab
Michał Słomkowski <m.slomkowski@gmail.com>
2014-10-12 14:56:43 +0200

    client: Arm visualizer add drawing robot base.

--- host/szark_client/src/main/java/eu/slomkowski/szark/client/gui/ArmVisualizer.java
+++ host/szark_client/src/main/java/eu/slomkowski/szark/client/gui/ArmVisualizer.java
@@ -1,36 +1,27 @@
 package eu.slomkowski.szark.client.gui;
 
-import eu.slomkowski.szark.client.HardcodedConfiguration;
 import eu.slomkowski.szark.client.status.Status;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.Point2D;
 
 public class ArmVisualizer extends JPanel {
 
-	final double SHOULDER_LENGTH = 53.5;
-	final double SHOULDER_MIN_ANGLE = -41;
-	final double SHOULDER_ANGLE_RANGE = 101;
-	final double SHOULDER_MAX_POSITION = 79;
+	private final double SHOULDER_LENGTH = 53.5;
+	private final double SHOULDER_MIN_ANGLE = -41;
+	private final double SHOULDER_ANGLE_RANGE = 101;
+	private final double SHOULDER_MAX_POSITION = 79;
 
-	final double ELBOW_LENGTH = 65.5;
-	final double ELBOW_MIN_ANGLE = -20;
-	final double ELBOW_ANGLE_RANGE = 105;
-	final double ELBOW_MAX_POSITION = 105;
+	private final double ELBOW_LENGTH = 65.5;
+	private final double ELBOW_MIN_ANGLE = -20;
+	private final double ELBOW_ANGLE_RANGE = 105;
+	private final double ELBOW_MAX_POSITION = 105;
 
-	final double MACHINE_WIDTH = 51;
-	final double MACHINE_HEIGHT = 17.5;
-	final double MACHINE_SHOULDER_BASE_OFFSET = 14.5;
+	private final double MACHINE_WIDTH = 51;
+	private final double MACHINE_HEIGHT = 17.5;
+	private final double MACHINE_SHOULDER_BASE_OFFSET = 36.5;
 
 	private Status status;
-
-	public ArmVisualizer(Status status) {
-		this();
-
-		this.status = status;
-	}
 
 	public ArmVisualizer() {
 		setPreferredSize(new Dimension(250, 250));
@@ -56,7 +47,7 @@ public class ArmVisualizer extends JPanel {
 	public void paintComponent(Graphics graphics) {
 		super.paintComponent(graphics);
 
-		if (!isEnabled() || (status == null)) {
+		if (status == null) {
 			return;
 		}
 
@@ -64,19 +55,13 @@ public class ArmVisualizer extends JPanel {
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 
-		g.setStroke(new BasicStroke(5));
-		final double SCALE = 1.3;
-		//final AffineTransform afSave = g.getTransform();
+		final double SCALE = 1.5;
 
-		final Dimension lo = new Dimension(getWidth() - 10, getHeight() - 23);
-
-		g.setColor(Color.black);
-
-		Point shoulderBase = new Point(lo.width / 2, lo.height);
-
-		// g.fillRect(34, 34, 55, 33);
-		// g.fillRect(up.width, up.height, lo.width, lo.height);
-		g.fillArc(shoulderBase.x - 25, shoulderBase.y - 5, 50, 50, 0, 180);
+		if (isEnabled()) {
+			g.setColor(Color.black);
+		} else {
+			g.setColor(Color.gray);
+		}
 
 		double minShoulderAngle = Math.toRadians(SHOULDER_MIN_ANGLE);
 		double shoulderAngleRange = Math.toRadians(SHOULDER_ANGLE_RANGE);
@@ -88,24 +73,39 @@ public class ArmVisualizer extends JPanel {
 		double elbowPart = (double) status.joints.elbow.getPosition() / ELBOW_MAX_POSITION;
 		double elbowAngle = elbowPart * elbowAngleRange + minElbowAngle;
 
-		Point elbowBase = new Point();
-		elbowBase.setLocation(shoulderBase.x - SCALE * SHOULDER_LENGTH * Math.sin(shoulderAngle),
-				shoulderBase.y - SCALE * SHOULDER_LENGTH * Math.cos(shoulderAngle));
 
-		Point gripperBase = new Point();
-		gripperBase.setLocation(elbowBase.x - SCALE * ELBOW_LENGTH * Math.sin(elbowAngle),
-				elbowBase.y - SCALE * ELBOW_LENGTH * Math.cos(elbowAngle));
-		
+		Point basePoint = new Point(
+				getWidth() / 2,
+				getHeight() - 70);
+
+		Point shoulderBase = new Point(
+				(int) Math.round(basePoint.x + SCALE * MACHINE_SHOULDER_BASE_OFFSET),
+				basePoint.y);
+
+		Point elbowBase = new Point(
+				(int) Math.round(shoulderBase.x - SCALE * SHOULDER_LENGTH * Math.sin(shoulderAngle)),
+				(int) Math.round(shoulderBase.y - SCALE * SHOULDER_LENGTH * Math.cos(shoulderAngle)));
+
+		Point gripperBase = new Point(
+				(int) Math.round(elbowBase.x - SCALE * ELBOW_LENGTH * Math.sin(elbowAngle)),
+				(int) Math.round(elbowBase.y - SCALE * ELBOW_LENGTH * Math.cos(elbowAngle)));
+
+		g.setStroke(new BasicStroke(5));
+
+		fillOval(g, shoulderBase, 12);
 		g.drawLine(shoulderBase.x, shoulderBase.y, elbowBase.x, elbowBase.y);
 
+		fillOval(g, elbowBase, 12);
 		g.drawLine(elbowBase.x, elbowBase.y, gripperBase.x, gripperBase.y);
 
-		//g.drawLine(basePoint.x, b
+		g.setStroke(new BasicStroke(3));
 
-		//final Rectangle p = new Rectangle(lo.width / 2, lo.height, 5, 80);
+		g.drawRect(basePoint.x, basePoint.y, (int) Math.round(SCALE * MACHINE_WIDTH),
+				(int) Math.round(SCALE * MACHINE_HEIGHT));
+	}
 
-		//g.draw(p);
-
-		//g.setTransform(afSave);
+	private void fillOval(Graphics g, Point center, int diameter) {
+		int radius = (int) Math.round(diameter / 2.0);
+		g.fillOval(center.x - radius, center.y - radius, diameter, diameter);
 	}
 }
