80220f837c3c8a542a1102e0c2fda4caf182ae1f
Michał Słomkowski <m.slomkowski@gmail.com>
2015-12-18 00:58:37 +0100

    server: Add zeroing request queue after request 0.

--- server/control_server/src/RequestQueuer.cpp
+++ server/control_server/src/RequestQueuer.cpp
@@ -1,11 +1,12 @@
-#include <chrono>
-#include <iomanip>
-#include <boost/format.hpp>
-#include <pthread.h>
-
 #include "utils.hpp"
 #include "IRequestProcessor.hpp"
 #include "RequestQueuer.hpp"
+
+#include <pthread.h>
+
+#include <chrono>
+#include <iomanip>
+#include <boost/format.hpp>
 
 using namespace std;
 using namespace boost;
@@ -24,175 +25,178 @@ constexpr bool REQUEST_QUEUE_OVERFLOW_BEHAVIOR = false;
 WALLAROO_REGISTER(RequestQueuer);
 
 processing::RequestQueuer::RequestQueuer()
-		: logger(log4cpp::Category::getInstance("RequestQueuer")),
-		  requestProcessors("requestProcessors", RegistrationToken()),
-		  jsonReader(Json::Reader(Json::Features::strictMode())) {
+        : logger(log4cpp::Category::getInstance("RequestQueuer")),
+          requestProcessors("requestProcessors", RegistrationToken()),
+          jsonReader(Json::Reader(Json::Features::strictMode())) {
 }
 
 void processing::RequestQueuer::Init() {
-	requestProcessorExecutorThread.reset(new thread(&RequestQueuer::requestProcessorExecutorThreadFunction, this));
-	int result = pthread_setname_np(requestProcessorExecutorThread->native_handle(), "reqProcExec");
-	if (result != 0) {
-		logger.error((format("Cannot set thread name: %s.") % strerror(result)).str());
-	}
-	logger.notice("Instance created.");
+    requestProcessorExecutorThread.reset(new thread(&RequestQueuer::requestProcessorExecutorThreadFunction, this));
+    int result = pthread_setname_np(requestProcessorExecutorThread->native_handle(), "reqProcExec");
+    if (result != 0) {
+        logger.error((format("Cannot set thread name: %s.") % strerror(result)).str());
+    }
+    logger.notice("Instance created.");
 }
 
 processing::RequestQueuer::~RequestQueuer() {
-	requestsMutex.lock();
-	finishCycleThread = true;
-	requestsMutex.unlock();
+    requestsMutex.lock();
+    finishCycleThread = true;
+    requestsMutex.unlock();
 
-	logger.notice("Waiting for processor thread to be stopped.");
-	cv.notify_one();
-	if (requestProcessorExecutorThread.get() != nullptr) {
-		requestProcessorExecutorThread->join();
-	}
+    logger.notice("Waiting for processor thread to be stopped.");
+    cv.notify_one();
+    if (requestProcessorExecutorThread.get() != nullptr) {
+        requestProcessorExecutorThread->join();
+    }
 
-	logger.notice("Instance destroyed.");
+    logger.notice("Instance destroyed.");
 }
 
 long processing::RequestQueuer::addRequest(string requestString, boost::asio::ip::address address) {
-	unique_lock<mutex> lk(requestsMutex);
+    unique_lock<mutex> lk(requestsMutex);
 
-	logger.debug((format("Received request with the size of %d bytes.") % requestString.length()).str());
+    logger.debug((format("Received request with the size of %d bytes.") % requestString.length()).str());
 
-	if (requests.size() == REQUEST_QUEUE_MAX_SIZE and REQUEST_QUEUE_OVERFLOW_BEHAVIOR) {
-		logger.warn((format("Requests queue is full (%d). Skipping request.") % REQUEST_QUEUE_MAX_SIZE).str());
-		return INVALID_MESSAGE;
-	}
+    if (requests.size() == REQUEST_QUEUE_MAX_SIZE and REQUEST_QUEUE_OVERFLOW_BEHAVIOR) {
+        logger.warn((format("Requests queue is full (%d). Skipping request.") % REQUEST_QUEUE_MAX_SIZE).str());
+        return INVALID_MESSAGE;
+    }
 
-	Json::Value req;
-	bool parseSuccess = jsonReader.parse(requestString, req);
+    Json::Value req;
+    bool parseSuccess = jsonReader.parse(requestString, req);
 
-	if (not parseSuccess) {
-		logger.error("Received request is not valid JSON document. See NOTICE for details.");
-		logger.notice("Details of the invalid request: " + jsonReader.getFormattedErrorMessages());
-		return INVALID_MESSAGE;
-	}
+    if (not parseSuccess) {
+        logger.error("Received request is not valid JSON document. See NOTICE for details.");
+        logger.notice("Details of the invalid request: " + jsonReader.getFormattedErrorMessages());
+        return INVALID_MESSAGE;
+    }
 
-	if (not req["serial"].isInt()) {
-		logger.error("Request does not contain valid serial.");
-		return INVALID_MESSAGE;
-	}
+    if (not req["serial"].isInt()) {
+        logger.error("Request does not contain valid serial.");
+        return INVALID_MESSAGE;
+    }
 
-	auto serial = req["serial"].asInt();
+    auto serial = req["serial"].asInt();
 
-	if (serial != 0 and serial <= lastSerial) {
-		logger.warn((format("Request has too old serial (%d). Skipping.") % serial).str());
-		return INVALID_MESSAGE;
-	}
+    if (serial != 0 and serial <= lastSerial) {
+        logger.warn((format("Request has too old serial (%d). Skipping.") % serial).str());
+        return INVALID_MESSAGE;
+    }
 
-	if (requests.size() == REQUEST_QUEUE_MAX_SIZE) {
-		logger.warn(
-				(format("Requests queue is full (%d). removing the oldest one.") % REQUEST_QUEUE_MAX_SIZE).str());
+    if (requests.size() == REQUEST_QUEUE_MAX_SIZE) {
+        logger.warn(
+                (format("Requests queue is full (%d). removing the oldest one.") % REQUEST_QUEUE_MAX_SIZE).str());
 
-		long id;
+        long id;
 
-		tie(id, ignore, ignore) = requests.top();
+        tie(id, ignore, ignore) = requests.top();
 
-		requests.pop();
+        requests.pop();
 
-		if (rejectedRequestRemover == nullptr) {
-			logger.error("Cannot remove rejected request. No RejectedRequestRemover set.");
-		} else {
-			rejectedRequestRemover(id);
-		}
-	}
+        if (rejectedRequestRemover == nullptr) {
+            logger.error("Cannot remove rejected request. No RejectedRequestRemover set.");
+        } else {
+            rejectedRequestRemover(id);
+        }
+    }
 
-	if (serial == 0) {
-		logger.notice("Request has the serial = 0, resetting counter.");
-		lastSerial = 0;
-	}
+    if (serial == 0) {
+        logger.notice("Request has the serial = 0, resetting counter and clearing queue.");
+        for (unsigned int i = 0; i < requests.size(); ++i) {
+            requests.pop();
+        }
+        lastSerial = 0;
+    }
 
-	long id = nextId();
+    long id = nextId();
 
-	requests.push(make_tuple(id, address, req));
+    requests.push(make_tuple(id, address, req));
 
-	logger.info(
-			(format("Pushed request with the serial %d. Queue size: %d.") % serial % requests.size()).str());
+    logger.info(
+            (format("Pushed request with the serial %d. Queue size: %d.") % serial % requests.size()).str());
 
-	cv.notify_one();
+    cv.notify_one();
 
-	return id;
+    return id;
 }
 
 int processing::RequestQueuer::getNumOfMessages() {
-	unique_lock<mutex> lk(requestsMutex);
-	return requests.size();
+    unique_lock<mutex> lk(requestsMutex);
+    return requests.size();
 }
 
 int processing::RequestQueuer::getNumOfProcessors() {
-	return requestProcessors.size();
+    return requestProcessors.size();
 }
 
 long processing::RequestQueuer::nextId() {
-	static long id = 1;
-	id++;
-	return id;
+    static long id = 1;
+    id++;
+    return id;
 }
 
 void processing::RequestQueuer::requestProcessorExecutorThreadFunction() {
-	while (true) {
-		unique_lock<mutex> lk(requestsMutex);
+    while (true) {
+        unique_lock<mutex> lk(requestsMutex);
 
-		if (requests.empty()) {
-			cv.wait(lk, [&]() {
-				return finishCycleThread or (not requests.empty());
-			});
-		}
+        if (requests.empty()) {
+            cv.wait(lk, [&]() {
+                return finishCycleThread or (not requests.empty());
+            });
+        }
 
-		if (finishCycleThread) {
-			return;
-		}
+        if (finishCycleThread) {
+            return;
+        }
 
-		long id;
-		asio::ip::address addr;
-		Json::Value req;
+        long id;
+        asio::ip::address addr;
+        Json::Value req;
 
-		tie(id, addr, req) = requests.top();
-		requests.pop();
+        tie(id, addr, req) = requests.top();
+        requests.pop();
 
-		lk.unlock();
+        lk.unlock();
 
-		auto serial = req["serial"].asInt();
+        auto serial = req["serial"].asInt();
 
-		if (serial < lastSerial) {
-			logger.warn((format("Request has too old serial (%d). Skipping (from executor).") % serial).str());
-			continue;
-		} else {
-			lastSerial = serial;
-		}
+        if (serial < lastSerial) {
+            logger.warn((format("Request has too old serial (%d). Skipping (from executor).") % serial).str());
+            continue;
+        } else {
+            lastSerial = serial;
+        }
 
-		logger.info((format("Executing request with serial %d from %s.") % serial % addr.to_string()).str());
+        logger.info((format("Executing request with serial %d from %s.") % serial % addr.to_string()).str());
 
-		Json::Value response;
+        Json::Value response;
 
-		response["serial"] = serial;
+        response["serial"] = serial;
 
-		auto execTimeMicroseconds = common::utils::measureTime<chrono::microseconds>([&]() {
-			for (auto proc : requestProcessors) {
-				std::shared_ptr<IRequestProcessor>(proc)->process(req, addr, response);
-			}
-		});
+        auto execTimeMicroseconds = common::utils::measureTime<chrono::microseconds>([&]() {
+            for (auto proc : requestProcessors) {
+                std::shared_ptr<IRequestProcessor>(proc)->process(req, addr, response);
+            }
+        });
 
-		response["timestamp"] = common::utils::getTimestamp();
+        response["timestamp"] = common::utils::getTimestamp();
 
-		logger.info((format("Request %d executed in %d us.") % serial % execTimeMicroseconds).str());
+        logger.info((format("Request %d executed in %d us.") % serial % execTimeMicroseconds).str());
 
-		bool skipResponse = req["skip_response"].asBool();
+        bool skipResponse = req["skip_response"].asBool();
 
-		if (skipResponse) {
-			logger.debug("Skipping response sending.");
-		} else {
-			logger.debug(string("Response in JSON: ") + jsonWriter.write(response));
-		}
+        if (skipResponse) {
+            logger.debug("Skipping response sending.");
+        } else {
+            logger.debug(string("Response in JSON: ") + jsonWriter.write(response));
+        }
 
-		if (responseSender == nullptr) {
-			logger.error("Cannot send response. No ResponseSender set.");
-		} else {
-			responseSender(id, jsonWriter.write(response), not skipResponse);
-		}
-	}
+        if (responseSender == nullptr) {
+            logger.error("Cannot send response. No ResponseSender set.");
+        } else {
+            responseSender(id, jsonWriter.write(response), not skipResponse);
+        }
+    }
 }
 
--- server/control_server/src/RequestQueuer.hpp
+++ server/control_server/src/RequestQueuer.hpp
@@ -1,5 +1,13 @@
-#ifndef REQUESTQUEUER_HPP_
-#define REQUESTQUEUER_HPP_
+#pragma once
+
+#include "IRequestProcessor.hpp"
+
+#include <log4cpp/Category.hh>
+#include <wallaroo/registered.h>
+#include <json/value.h>
+#include <json/reader.h>
+#include <json/writer.h>
+#include <boost/asio/ip/address.hpp>
 
 #include <string>
 #include <list>
@@ -10,97 +18,86 @@
 #include <thread>
 #include <condition_variable>
 
-#include <log4cpp/Category.hh>
-#include <wallaroo/registered.h>
-#include <json/value.h>
-#include <json/reader.h>
-#include <json/writer.h>
-#include <boost/asio/ip/address.hpp>
-
-#include "IRequestProcessor.hpp"
-
 namespace processing {
 
-	typedef std::function<void(long, std::string, bool)> ResponseSender;
-	typedef std::function<void(long)> RejectedRequestRemover;
-	typedef std::tuple<long, boost::asio::ip::address, Json::Value> Request;
+    typedef std::function<void(long, std::string, bool)> ResponseSender;
+    typedef std::function<void(long)> RejectedRequestRemover;
+    typedef std::tuple<long, boost::asio::ip::address, Json::Value> Request;
 
-	constexpr long INVALID_MESSAGE = -1;
+    constexpr long INVALID_MESSAGE = -1;
 
-	class IRequestQueuer {
-	public:
-		/**
-		* Adds the request to the queue.
-		* @param request text of the request in JSON format
-		* @param address IP address of the client
-		* @return true if the request was added to the queue
-		*/
-		virtual long addRequest(std::string request, boost::asio::ip::address address) = 0;
+    class IRequestQueuer {
+    public:
+        /**
+        * Adds the request to the queue.
+        * @param request text of the request in JSON format
+        * @param address IP address of the client
+        * @return true if the request was added to the queue
+        */
+        virtual long addRequest(std::string request, boost::asio::ip::address address) = 0;
 
-		virtual int getNumOfMessages() = 0;
+        virtual int getNumOfMessages() = 0;
 
-		virtual int getNumOfProcessors() = 0;
+        virtual int getNumOfProcessors() = 0;
 
-		virtual void setResponseSender(ResponseSender s) = 0;
+        virtual void setResponseSender(ResponseSender s) = 0;
 
-		virtual void setRejectedRequestRemover(RejectedRequestRemover r) = 0;
+        virtual void setRejectedRequestRemover(RejectedRequestRemover r) = 0;
 
-		virtual ~IRequestQueuer() = default;
-	};
+        virtual ~IRequestQueuer() = default;
+    };
 
-	class RequestValueComparer {
-	public:
-		bool operator()(const Request &r1, const Request &r2) const {
-			return std::get<2>(r1)["serial"].asInt() > std::get<2>(r2)["serial"].asInt();
-		}
-	};
+    class RequestValueComparer {
+    public:
+        bool operator()(const Request &r1, const Request &r2) const {
+            return std::get<2>(r1)["serial"].asInt() > std::get<2>(r2)["serial"].asInt();
+        }
+    };
 
-	class RequestQueuer : public wallaroo::Device, public IRequestQueuer {
-	public:
-		RequestQueuer();
+    class RequestQueuer : public wallaroo::Device, public IRequestQueuer {
+    public:
+        RequestQueuer();
 
-		~RequestQueuer();
+        ~RequestQueuer();
 
-		virtual long addRequest(std::string requestString, boost::asio::ip::address address);
+        virtual long addRequest(std::string requestString, boost::asio::ip::address address);
 
-		virtual int getNumOfMessages();
+        virtual int getNumOfMessages();
 
-		virtual int getNumOfProcessors();
+        virtual int getNumOfProcessors();
 
-		void setResponseSender(ResponseSender s) {
-			responseSender = s;
-		}
+        void setResponseSender(ResponseSender s) {
+            responseSender = s;
+        }
 
-		void setRejectedRequestRemover(RejectedRequestRemover r) {
-			rejectedRequestRemover = r;
-		}
+        void setRejectedRequestRemover(RejectedRequestRemover r) {
+            rejectedRequestRemover = r;
+        }
 
-	private:
-		log4cpp::Category &logger;
+    private:
+        log4cpp::Category &logger;
 
-		wallaroo::Plug<IRequestProcessor, wallaroo::collection> requestProcessors;
+        wallaroo::Plug<IRequestProcessor, wallaroo::collection> requestProcessors;
 
-		std::mutex requestsMutex;
-		std::priority_queue<Request, std::vector<Request>, RequestValueComparer> requests;
-		volatile long lastSerial = 0;
+        std::mutex requestsMutex;
+        std::priority_queue<Request, std::vector<Request>, RequestValueComparer> requests;
+        volatile long lastSerial = 0;
 
-		std::unique_ptr<std::thread> requestProcessorExecutorThread;
-		std::condition_variable cv;
-		volatile bool finishCycleThread = false;
+        std::unique_ptr<std::thread> requestProcessorExecutorThread;
+        std::condition_variable cv;
+        volatile bool finishCycleThread = false;
 
-		Json::Reader jsonReader;
-		Json::FastWriter jsonWriter;
+        Json::Reader jsonReader;
+        Json::FastWriter jsonWriter;
 
-		ResponseSender responseSender;
-		RejectedRequestRemover rejectedRequestRemover;
+        ResponseSender responseSender;
+        RejectedRequestRemover rejectedRequestRemover;
 
-		void Init();
+        void Init();
 
-		void requestProcessorExecutorThreadFunction();
+        void requestProcessorExecutorThreadFunction();
 
-		long nextId();
-	};
+        long nextId();
+    };
 
 } /* namespace processing */
-
-#endif /* REQUESTQUEUER_HPP_ */
