b73fd93492468115df5660e6bd601335f5a3034f
Michał Słomkowski <m.slomkowski@gmail.com>
2016-02-13 18:59:51 +0100

    client: Migrate to logback.

--- client/desktop_client/src/main/java/eu/slomkowski/szark/client/SzarkClient.java
+++ client/desktop_client/src/main/java/eu/slomkowski/szark/client/SzarkClient.java
@@ -2,6 +2,7 @@ package eu.slomkowski.szark.client;
 
 import eu.slomkowski.szark.client.gui.MainWindowLogic;
 import org.pushingpixels.substance.api.skin.SubstanceTwilightLookAndFeel;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import javax.swing.*;
 
@@ -9,6 +10,9 @@ class SzarkClient implements Runnable {
 
     @Override
     public void run() {
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
+
         try {
             UIManager.setLookAndFeel(new SubstanceTwilightLookAndFeel());
         } catch (UnsupportedLookAndFeelException e) {
