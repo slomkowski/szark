1e75976e93a97ed495d217b8d7e46dca66fa15e7
Michał Słomkowski <m.slomkowski@gmail.com>
2016-04-11 23:35:06 +0200

    camera: Add input support to Python camera viewer.

--- server/tests/image_receiver.py
+++ server/tests/image_receiver.py
@@ -12,29 +12,43 @@ import time
 import cv2
 import numpy
 
-DEFAULT_PORT = 10192
 
-if len(sys.argv) < 2:
+def print_usage():
     print("Usage:\n"
-          "%s {host} [port]\n"
-          "port = %d by default\n\n"
-          "press 's' to save image, 'q' to exit" % (__file__, DEFAULT_PORT))
+          "%s {host} {port} [input identifiers]\n\n"
+          "press 's' to save image, 'q' to exit, space switches between inputs" % __file__)
+
+
+if len(sys.argv) < 3:
+    print_usage()
     sys.exit(1)
 
 udpAddress = sys.argv[1]
-udpPort = int(sys.argv[2]) if len(sys.argv) >= 3 else DEFAULT_PORT
+udpPort = int(sys.argv[2])
+
+input_identifiers = sys.argv[3:] if len(sys.argv) > 3 else ['default']
+current_input_no = 0
+current_serial = 1
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(0.7)
 
-req_header = {
-    "serial": 5,
-    "compress": False,
-    "drawHud": True
-}
+print_usage()
 
 while True:
-    sock.sendto(json.dumps(req_header), (udpAddress, udpPort))
-    (data, addr) = sock.recvfrom(60000)
+    req_header = {
+        "serial": current_serial,
+        "compress": False,
+        "drawHud": True,
+        "input": input_identifiers[current_input_no]
+    }
+    current_serial += 1
+
+    try:
+        sock.sendto(json.dumps(req_header), (udpAddress, udpPort))
+        (data, addr) = sock.recvfrom(60000)
+    except socket.timeout:
+        continue
 
     for _headerEnd in range(max(len(data), 300)):
         if ord(data[_headerEnd]) == 0:
@@ -50,6 +64,9 @@ while True:
     if k == ord('s'):
         with open('dump-%d.jpg' % time.time(), 'wb') as f:
             f.write(data[headerEnd + 1:])
+    elif k == ord(' '):
+        current_input_no = (current_input_no + 1) % len(input_identifiers)
+        print("Switched input to %s." % input_identifiers[current_input_no])
     elif k == 27 or k == ord('q'):
         cv2.destroyAllWindows()
         break
